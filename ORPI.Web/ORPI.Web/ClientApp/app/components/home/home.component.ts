92302bcfaf72f11f0d3556b89c356bcd29f4af78
Bogdan <bogdanshcherbak56@gmail.com>
2018-07-04 01:47:05 +0300

    ю

--- ORPI.Web/ORPI.Web/ClientApp/app/components/home/home.component.ts
+++ ORPI.Web/ORPI.Web/ClientApp/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked } from '@angular/core';
+import { ScrollHelper } from '../helpers/scrollhelper/scrollhelper';
 
 @Component({
     selector: 'home',
@@ -6,4 +7,24 @@ import { Component } from '@angular/core';
 	styleUrls: ['./home.component.css', './vendor/bootstrap/css/bootstrap.min.css','./css/creative.min.css']
 })
 export class HomeComponent {
+	private scrollHelper: ScrollHelper = new ScrollHelper();
+	ngAfterViewChecked() {
+		this.scrollHelper.doScroll();
+	}
+
+	scrollToContact() {
+		this.scrollHelper.scrollToFirst('scrollcontact');
+		this.scrollHelper.doScroll();
+	}
+
+	scrollToMain() {
+		this.scrollHelper.scrollToFirst('masthead');
+		this.scrollHelper.doScroll();
+	}
+
+	scrollToAbout() {
+		this.scrollHelper.scrollToFirst('bg-primary');
+		this.scrollHelper.doScroll();
+	}
+
 }
